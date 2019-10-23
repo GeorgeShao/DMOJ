0f13836f95c96c4ccc1c4380c92a58b339f3f588
George Shao <GeorgeShao123@gmail.com>
2019-10-22 22:47:21 -0400

    Added solution to BackFromSummer19P1.py

--- DMOJ-Python/BackFromSummer19P1.py
+++ DMOJ-Python/BackFromSummer19P1.py
@@ -1,12 +1,14 @@
-orphanages, kids = map(int, input().split())
+orphanages, kids = input().split()
+orphanages = int(orphanages)
+kids = int(kids)
 
-num_of_bad = []
+num_of_bad = [None]*orphanages
 
 for i in range(orphanages):
-    kid_scores = list(map(int, input().split()))
-    print(kid_scores)
+    num_of_bad[i] = 0
+    kid_scores = map(int, input().split())
     for score in kid_scores:
         if score == 1 or score == 10:
             num_of_bad[i] += 1
 
-print(num_of_bad.index(min(num_of_bad)))
+print(num_of_bad.index(min(num_of_bad)) + 1)
