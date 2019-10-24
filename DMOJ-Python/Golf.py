2e57865b703492e47101b9d7022be66970996e6d
George Shao <GeorgeShao123@gmail.com>
2019-10-24 18:32:27 -0400

    Attempted solution of Golf.py

---
+++ DMOJ-Python/Golf.py
@@ -0,0 +1,28 @@
+dist = int(input())
+num_dist = int(input())
+dists = [None] * num_dist
+
+for i in range(num_dist):
+    dists[i] = int(input())
+
+dists.sort()
+
+strokes = 1
+
+def return_left(left):
+    global strokes, dists
+    flag = None
+    for distance in dists:
+        if min(dists) > left:
+            print("Roberta acknowledges defeat.")
+            exit()
+        if distance == left:  
+            return distance
+        if distance < left:
+            flag = distance
+    temp_distance = round(left - flag)
+    strokes += 1
+    return [flag] + [return_left(temp_distance)]
+
+return_left(dist)
+print(f"Roberta wins in {strokes} strokes.")
