db1296e06cf34b029b5d439afc4efdab363d2e00
George Shao <GeorgeShao123@gmail.com>
2019-11-04 19:11:53 -0500

    Worked on CCC_10_S1.py

--- CCC_10_S1.py
+++ CCC_10_S1.py
@@ -1,51 +1,30 @@
-# PARTIALLY WORKING
+# NOT WORKING
 
 import sys
 
-computers = int(input())
+computers = int(sys.stdin.readline())
+scores = {}
 
-best_score = 0
-second_best_score = 0
-
-best_computer = ""
-second_best_computer = ""
+if computers == 0:
+    sys.exit()
 
 for i in range(computers):
     name, r, s, d = sys.stdin.readline().split()
+    name = str(name)
     r = int(r)
     s = int(s)
     d = int(d)
-
     score = 2*r + 3*s + d
+    scores[name] = score
 
-    if score > best_score:
-        second_best_score = best_score
-        best_score = score
-        second_best_computer = best_computer
-        best_computer = name
-    elif score > second_best_score:
-        second_best_score = score
-        second_best_computer = name
-    elif score == best_score and score == second_best_score:
-        if name < best_computer:
-            second_best_score = best_score
-            best_score = score
-            second_best_computer = best_computer
-            best_computer = name
-        elif name < second_best_computer:
-            second_best_score = score
-            second_best_computer = name
-    elif score == best_score:
-        if score > second_best_score:
-            if name < best_computer:
-                second_best_score = best_score
-                best_score = score
-                second_best_computer = best_computer
-                best_computer = name
-    elif score == second_best_score:
-        if name < second_best_computer:
-            second_best_score = score
-            second_best_computer = name
+if computers == 1:
+    print((max(scores.items(), key=lambda x: x[1]))[0])
+    sys.exit()
 
-print(best_computer)
-print(second_best_computer)
+first_place = (max(scores.items(), key=lambda x: x[1]))[0]
+print(first_place)
+
+del scores[first_place]
+
+second_place = (max(scores.items(), key=lambda x: x[1]))[0]
+print(second_place)
