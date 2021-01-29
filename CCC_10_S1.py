d4a8d215953f6adf2159c4a275a18b4ea4aaa84c
George Shao <georgeshao123@gmail.com>
2021-01-29 12:20:40 -0500

    Finally finished CCC_10_S1.py after 1 year after being stuck on a small issue for 1.5 years

--- CCC_10_S1.py
+++ CCC_10_S1.py
@@ -4,6 +4,7 @@ import sys
 
 computers = int(sys.stdin.readline())
 scores = {}
+perm = {}
 
 if computers == 0:
     sys.exit()
@@ -16,15 +17,23 @@ for i in range(computers):
     d = int(d)
     score = 2*r + 3*s + d
     scores[name] = score
+    perm[name] = score
 
 if computers == 1:
     print((max(scores.items(), key=lambda x: x[1]))[0])
     sys.exit()
 
 first_place = (max(scores.items(), key=lambda x: x[1]))[0]
-print(first_place)
 
 del scores[first_place]
 
 second_place = (max(scores.items(), key=lambda x: x[1]))[0]
-print(second_place)
+
+if perm[first_place] == perm[second_place]:
+    temp = [first_place, second_place]
+    temp.sort()
+    print(temp[0])
+    print(temp[1])
+else:
+    print(first_place)
+    print(second_place)
