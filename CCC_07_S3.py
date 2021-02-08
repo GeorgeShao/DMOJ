60659e222119fd43f65960c7aeed89c08621d695
George Shao <georgeshao123@gmail.com>
2021-02-08 14:29:31 -0500

    Mostly correct answers

---
+++ CCC_07_S3.py
@@ -0,0 +1,58 @@
+from sys import stdin
+
+num_pairs = int(stdin.readline().replace('\n', ''))
+graph = {}
+inp = ""
+
+for i in range(num_pairs):
+    inp = stdin.readline().replace('\n', '').split()
+    first = int(inp[0])
+    second = int(inp[1])
+
+    try:
+        new_set = graph[first]
+    except:
+        new_set = set()
+    new_set.add(second)
+    graph[first] = new_set
+
+
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
+
+
+def shortest_path(graph, start, goal):
+    try:
+        return next(bfs_paths(graph, start, goal))
+    except StopIteration:
+        return None
+
+
+while True:
+    inp = stdin.readline().replace('\n', '').split()
+    first = int(inp[0])
+    second = int(inp[1])
+
+    if first == 0 and second == 0:
+        break
+
+    # first_paths_all = bfs_paths(graph, first, second)
+    # second_paths_all = bfs_paths(graph, second, first)
+
+    first_path = shortest_path(graph, first, second)
+    second_path = shortest_path(graph, second, first)
+
+    if bool(first_path) and bool(second_path):
+        if len(first_path) <= len(second_path):
+            print(f'Yes {len(first_path)-2}')
+        else:
+            print(f'Yes {len(second_path)-2}')
+    else:
+        print('No')
